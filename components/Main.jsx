bdb25fe07ad186a77b396d874cb477be210f547b
Gabriel <ahrensgabriel09@gmail.com>
2022-08-30 22:24:17 -0300

    Change p tag of the line 11

---
+++ components/Main.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET'S BUILD SOMETHING LEGENDARY TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#5651e5]">Gabriel</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A FullStack Web Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            I'm a Fullstack developer, BackEnd oriented. I have the ability to
+            develop both individual and group web projects. I'm focused on
+            building exceptional scalable and responsive web applications, while
+            learning new FrontEnd and BackEnd technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn color="#5651e5"/>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub color="#5651e5"/>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail color="#5651e5"/>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill color="#5651e5"/>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
