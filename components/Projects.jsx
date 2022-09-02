09f50556d3df660a78e0c63b8a4e44629886d3e5
Gabriel <ahrensgabriel09@gmail.com>
2022-09-02 01:21:29 -0300

    Add id property at the father div in the Projects component and respective ProjectItem URL

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,16 +6,16 @@ import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
         <h2 className="py-2">What I've Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem title={"HenryTravel"} image={henryImg} typeProject={"E-Commerce Project with ReactJS, NodeJS and Sequelize"} url={'https://proyecto-final-henry.vercel.app/'} />
-          <ProjectItem title={"CountryApp"} image={countryImg} typeProject={"ReactJS Project"} url={'https://country-app-theta.vercel.app/'} />
-          <ProjectItem title={"My Portfolio :)"} image={portfolioImg} typeProject={"NextJS Project"} url={'https://github.com/AhrensOG/Portfolio'} />
+          <ProjectItem title={"HenryTravel"} image={henryImg} typeProject={"E-Commerce Project with ReactJS, NodeJS and Sequelize"} url={'/henryTravel'} />
+          <ProjectItem title={"CountryApp"} image={countryImg} typeProject={"ReactJS Project"} url={'/countryApp'} />
+          <ProjectItem title={"My Portfolio :)"} image={portfolioImg} typeProject={"NextJS Project"} url={'/myPortfolio'} />
         </div>
       </div>
     </div>
