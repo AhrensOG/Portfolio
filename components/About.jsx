cd362353a24a3c6b6b01f3346ec98b092a93fe6c
Gabriel <ahrensgabriel09@gmail.com>
2022-09-02 01:22:38 -0300

    Add id property at the father div in the About component

--- components/About.jsx
+++ components/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import aboutImg from '../public/assets/about.jpg'
 
 const About = () => {
   return (
-    <div className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className=" max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
@@ -39,9 +40,9 @@ const About = () => {
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <img
+          <Image
             className="rounded-xl"
-            src="https://raw.githubusercontent.com/fireclint/portfolio-nextjs/main/public/assets/about.jpg"
+            src={aboutImg}
             alt="img"
           />
         </div>
