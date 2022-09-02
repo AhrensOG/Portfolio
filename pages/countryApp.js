d10edb1493f4c4a413f5aed32b49c50d8676f8de
Gabriel <ahrensgabriel09@gmail.com>
2022-09-02 01:34:27 -0300

    Add/feat a pages for each project

---
+++ pages/countryApp.js
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import React from "react";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+import propertyImage from "../public/assets/projects/countryApp-image.png";
+
+const countryApp = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={propertyImage}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Property Finder</h2>
+          <h3>React JS / Tailwind / Firebase</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This application was built using React JS, Redux, NodeJS, Express,
+            Sequelize and is hosted on heroku/vercel. Users are able to search
+            for countries and filter them based on continent, population or
+            tourist activity. They will be able to view country information with
+            related activities, as well as have the ability to create new
+            tourist activities. This is possible thanks to the COUNTRIES API and
+            a database developed with Sequelize. The application provides the
+            possibility to manipulate tourist activities (create, update and
+            delete).
+          </p>
+          <a
+            href="https://github.com/AhrensOG/CountryApp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Code</button>
+          </a>
+          <a
+            href="https://property-finder-development.web.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Demo</button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascript
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Firebase
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Google API
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Zillow API
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default countryApp;
---
+++ pages/henryTravel.js
@@ -0,0 +1,109 @@
+import Image from "next/image";
+import React from "react";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+import propertyImage from "../public/assets/projects/henryTravel-image.png";
+
+const henryTravel = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={propertyImage}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Henry Travel</h2>
+          <h3>ReactJS | Css | NodeJS | Sequelize</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This application was built using React JS, Redux,
+            NodeJS, Express, Sequelize and is hosted on heroku/vercel. Users are
+            able to search/filter tour packages based on a destination or date,
+            to retrieve a list of currently available packages. They will be
+            able to view the package information with the activities included,
+            as well as the ability to add new activities. User authentication is
+            available thanks to Auth0 so that you can register and login to your
+            account with an email address or Google account in order to save
+            your favorite tour packages and your shopping cart. This is possible
+            thanks to the API developed with NodeJS and a database developed
+            with Sequelize. The application also has two payment gateways
+            developed with Stripe and MercadoPago, as well as a Dashboard for
+            administrative use that gives the possibility to manipulate tour
+            packages and touristic activities (Create, update and a logical
+            deletion). Translated with www.DeepL.com/Translator (free version)
+          </p>
+          <a
+            href="https://github.com/bamioezequiel/proyecto-final-henry"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Code</button>
+          </a>
+          <a
+            href="https://proyecto-final-henry.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Demo</button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascrip
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> ReactJS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Redux
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Css
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> NodeJS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Express
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Sequelize
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Stripe
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> MercadoPago
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Auth0
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> NodeMailer
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default henryTravel;
