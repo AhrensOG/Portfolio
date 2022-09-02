9e4038b4615ca8e77ebf0d6dd77bf17c6afcb3bf
Gabriel <ahrensgabriel09@gmail.com>
2022-09-02 13:28:56 -0300

    Change src Image

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -83,7 +83,7 @@ const Skills = () => {
                 <div className="grid grid-cols-2 gap-4 justify-center items-center">
                   <div className="m-auto">
                     <Image
-                      src={`/../public/assets/skills/${s.iconName}-icon.png`}
+                      src={`/../public/assets/skills/${s.iconName}.png`}
                       alt="/"
                       width="64px"
                       height="64px"
