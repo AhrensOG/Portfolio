1298c3a94bd99453115a14ba0862b06907311596
Gabriel <ahrensgabriel09@gmail.com>
2022-09-05 16:45:32 -0300

    Change Project Overview

--- pages/myPortfolio.js
+++ pages/myPortfolio.js
@@ -1,74 +1,71 @@
-import Image from 'next/image';
-import React from 'react';
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
-import propertyImage from '../public/assets/projects/henryTravel-image.png';
+import Image from "next/image";
+import React from "react";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+import propertyImage from "../public/assets/projects/henryTravel-image.png";
 
 const myPortfolio = () => {
   return (
-    <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
         <Image
-          className='absolute z-1'
-          layout='fill'
-          objectFit='cover'
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
           src={propertyImage}
-          alt='/'
+          alt="/"
         />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>My Portfolio</h2>
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">My Portfolio</h2>
           <h3>Next JS | Javascrip | Tailwind </h3>
         </div>
       </div>
 
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4'>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            This app was built using React JS and is hosted on Firebase. Users
-            are able to search properties based on an Address, City, or ZIP code
-            to retrieve a list of active properties currently for sale. You will
-            be able to view property information as well as the specific
-            location of the property integrated with the Google Maps API. User
-            authentication is available so you can signup and signin to your
-            account with an email address in order to save your favorite
-            properties. This is made possible with Zillow API.
+            This application was built using NextJS, Tailwind and is hosted at
+            Vercel. Users will be able to read more about me. They will know my
+            projects, my skills and they will be able to see a demo or the code
+            in github. They can contact me through a form in the application or
+            through my social networks!
           </p>
           <Link
-            href='https://github.com/AhrensOG/Portfolio'
-            target='_blank'
-            rel='noreferrer'
+            href="https://github.com/AhrensOG/Portfolio"
+            target="_blank"
+            rel="noreferrer"
           >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+            <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </Link>
-          <Link href='/'>
-            <button className='px-8 py-2 mt-4'>Demo</button>
+          <Link href="/">
+            <button className="px-8 py-2 mt-4">Demo</button>
           </Link>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Javascript
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascript
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Next JS
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Next JS
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Tailwind
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
               </p>
             </div>
           </div>
         </div>
-        <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
         </Link>
       </div>
     </div>
   );
-}
+};
 
-export default myPortfolio
+export default myPortfolio;
