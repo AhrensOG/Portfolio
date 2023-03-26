d0718e33d395054567f35f174ab170e86d513d57
AhrensOG <ahrensgabriel09@gmail.com>
2023-03-26 17:21:40 -0300

    Add ONG project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import portfolioImg from "../public/assets/projects/portfolio-image.png";
+import somosMasImg from "../public/assets/projects/somosMas-image.png";
 import countryImg from '../public/assets/projects/countryApp-image.png'
 import henryImg from "../public/assets/projects/henryTravel-image.png";
 import newspaper from "../public/assets/projects/NewspaperImage.png";
@@ -16,7 +16,7 @@ const Projects = () => {
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem title={"HenryTravel"} image={henryImg} typeProject={"E-Commerce Project with ReactJS, NodeJS and Sequelize"} url={'/henryTravel'} />
           <ProjectItem title={"CountryApp"} image={countryImg} typeProject={"ReactJS Project"} url={'/countryApp'} />
-          <ProjectItem title={"My Portfolio :)"} image={portfolioImg} typeProject={"NextJS Project"} url={'/myPortfolio'} />
+          <ProjectItem title={"SomosMas ONG"} image={somosMasImg} typeProject={"ReactJS Project"} url={'/somosMas'} />
           <ProjectItem title={"Newspaper"} image={newspaper} typeProject={"NextJS Project with NodeJS and Firebase"} url={'/newspaper'} />
         </div>
       </div>
--- pages/somosMas.js
+++ pages/somosMas.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 import React from "react";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
-import propertyImage from "../public/assets/projects/henryTravel-image.png";
+import propertyImage from "../public/assets/projects/somosMas-image.png";
 
-const myPortfolio = () => {
+const somosMas = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[50vh] relative">
@@ -17,8 +17,8 @@ const myPortfolio = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">My Portfolio</h2>
-          <h3>Next JS | Javascrip | Tailwind </h3>
+          <h2 className="py-2">Somos Mas ONG</h2>
+          <h3>ReactJS | NodeJs | CSS Frameworks | Sequelize </h3>
         </div>
       </div>
 
@@ -27,20 +27,18 @@ const myPortfolio = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            This application was built using NextJS, Tailwind and is hosted at
-            Vercel. Users will be able to read more about me. They will know my
-            projects, my skills and they will be able to see a demo or the code
-            in github. They can contact me through a form in the application or
-            my social networks!
+          This application was built with ReactJS, NodeJS and Sequelize (PSQL) as main technologies.
+          The purpose of this project is to visualize the different activities performed by an ONG (Somos Mas).
+          The application has the ability to view information about the NGO, as well as news about their latest movements and testimonials of people who received help from Somos Mas. This app has an administrator panel in which it is possible to manage the different sections of the page, in addition to a form to enter or register on the page.
           </p>
           <Link
-            href="https://github.com/AhrensOG/Portfolio"
+            href="https://github.com/AhrensOG/OT289-client"
             target="_blank"
             rel="noreferrer"
           >
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </Link>
-          <Link href="/">
+          <Link href="https://somosmasong.vercel.app/">
             <button className="px-8 py-2 mt-4">Demo</button>
           </Link>
         </div>
@@ -49,13 +47,19 @@ const myPortfolio = () => {
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> JavaScript
+                <RiRadioButtonFill className="pr-1" /> NodeJS | Express
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> NextJS
+                <RiRadioButtonFill className="pr-1" /> AWS
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Tailwind
+                <RiRadioButtonFill className="pr-1" /> JWT
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> ReactJS | Redux
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> CSS3 | Bootstrap
               </p>
             </div>
           </div>
@@ -68,4 +72,4 @@ const myPortfolio = () => {
   );
 };
 
-export default myPortfolio;
+export default somosMas;
