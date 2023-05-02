747a05f9a15dbe11482aab61add856a6144637a5
AhrensOG <ahrensgabriel09@gmail.com>
2023-05-02 20:20:08 -0300

    Change domain of the countryApp

--- pages/countryApp.js
+++ pages/countryApp.js
@@ -45,7 +45,7 @@ const countryApp = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </Link>
           <Link
-            href="https://country-app-theta.vercel.app/"
+            href="https://country-app-project.vercel.app/"
             target="_blank"
             rel="noreferrer"
           >
